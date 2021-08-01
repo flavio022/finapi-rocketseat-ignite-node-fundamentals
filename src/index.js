ea649bf073803464d056673ebd12d8320ce63ef0
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2021-08-01 18:33:02 -0300

    criando middleware para validar se conta existe

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const { response } = require('express');
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+function verifyIfExistAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+    console.log(cpf);
+    const customer = customers.find((customer) => customer.cpf === cpf);
+    if (!customer) {
+        return response.status(400).json({ error: "Customer not found!" })
+    }
+    request.customer = customer;
+    return next();
+}
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+    const customersAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    )
+
+    if (customersAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" });
+    }
+
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statements: []
+    })
+    return response.json(customers);
+}
+);
+app.get("/statement", verifyIfExistAccountCPF, (request, response) => {
+    const { customer } = request;
+    return response.json(customer.statements);
+});
+app.listen(3000);
