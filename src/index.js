bf756bfd663454350aa92056ef0319c9d5d10965
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2021-08-01 23:13:22 -0300

    created withdraw method

--- src/index.js
+++ src/index.js
@@ -17,6 +17,19 @@ function verifyIfExistAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, opertation) => {
+        if (opertation.type === 'credit') {
+            return acc + opertation.amount;
+        } else {
+            return acc - opertation.amount;
+        }
+
+    }, 0)
+
+    return balance;
+}
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
     const customersAlreadyExists = customers.some(
@@ -60,5 +73,26 @@ app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
     return response.status(201).send();
 
 });
+
+app.post("/withdraw", verifyIfExistAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statements);
+
+    if (balance < amount) {
+        return response.status(400).json({ error: 'Insufficient founds!' })
+    }
+
+    const statementOpertation = {
+        amount,
+        created_at: new Date(),
+        type: 'debit'
+    }
+    customer.statements.push(statementOpertation);
+
+    return response.status(201).send();
+})
+
 
 app.listen(3000);
